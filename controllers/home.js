87d3b7ec4c868e9b66f02334e1942dd3d32ee4fd
adehenry <adefemiadelaja@gmail.com>
2022-09-15 13:13:54 -0700

    db connection

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 export const homePageController = (req, res) => {
   res.send(
-    `<h1 style="color:blue; text-align:center; font-family:verdana;">Server Home Page</h1>`
+    `<div  style="background-color: #000000;height:100%;width:100%"><h1 style="color:blue; text-align:center; font-family:verdana;">Server Home Page</h1></div>`
   );
 };
